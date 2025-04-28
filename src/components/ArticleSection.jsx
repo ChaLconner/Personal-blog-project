c1919fdacffd56d19f7f199382707f15082377ae
ChaL <chaluntonvipusanapas@gmail.com>
2025-04-28 13:11:21 +0700

    feat: implement interactive category filter buttons and mobile dropdown menu using useState

--- src/components/ArticleSection.jsx
+++ src/components/ArticleSection.jsx
@@ -1,7 +1,7 @@
+import { useState } from "react";
 import { Input } from "@/components/ui/input"
 import BlogCard from "./BlogCard";
 import { blogPosts } from "@/data/blogPosts";
-
 import {
     Select,
     SelectContent,
@@ -12,9 +12,9 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 
-const categories = ["Highlight", "Cat", "Inspiration", "General"];
-
 function ArticleSection() {
+    const categories = ["Highlight", "Cat", "Inspiration", "General"];
+    const [category, setCategory] = useState("Highlight");
     return (
         <section className="">
             {/* heading */}
@@ -22,13 +22,19 @@ function ArticleSection() {
                 <h3 className="font-600 p-[16px] text-[24px] sm:mb-[32px]">Latest articles</h3>
                 <div className="flex sm:justify-between items-center bg-[#EFEEEB] p-[16px] sm:rounded-[16px] sm:px-[24px] sm:py-[16px]" >
                     <div className="hidden md:flex space-x-2">
-                        {categories.slice(0).map((cat) => {
+                        {categories.slice(0).map((Highlight) => {
                             return (
                                 <button
-                                    key={cat}
-                                    className="px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium hover:bg-muted"
+                                    key={Highlight}
+                                    className={`px-4 py-2 rounded transition-colors text-sm font-medium ${
+                                        category === Highlight
+                                        ? "bg-gray-600 text-white" 
+                                            : "hover:bg-gray-300"
+                                    }`}
+                                    disabled={category === Highlight}
+                                    onClick={() => setCategory(Highlight)}
                                 >
-                                    {cat}
+                                    {Highlight}
                                 </button>
                             );
                         })}
@@ -37,7 +43,7 @@ function ArticleSection() {
                         <Input />
                         <div className="w-full sm:hidden">
                             <h1 className="text-muted-foreground mb-[4px]">Category</h1>
-                            <Select value="highlight">
+                            <Select value="category" onValueChange={(value) => setCategory(value)}>
                                 <SelectTrigger className="w-full py-3 rounded-sm text-muted-foreground">
                                     <SelectValue placeholder="Select category" />
                                 </SelectTrigger>
@@ -51,6 +57,19 @@ function ArticleSection() {
                                     })}
                                 </SelectContent>
                             </Select>
+                        </div>
+                        <div className="hidden smd:flex space-x-2">
+                            {categories.map((cat) => (
+                                <button
+                                    key={cat}
+                                    onClick={() => setCategory(cat)}
+                                    className={`px-4 py-3 transition-colors rounded-sm text-sm text-muted-foreground font-medium ${category === cat ? "bg-[#DAD6D1]" : "hover:bg-muted"
+                                        }`}
+                                >
+                                    {cat}
+                                </button>
+
+                            ))}
                         </div>
                     </div>
                 </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,12 @@
-import HamburgerMenu from "./ui/hamburgerMenu"
+import { Menu } from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function NavBar() {
     return (
@@ -7,16 +15,34 @@ function NavBar() {
             <a className="text-[44px] " href="/">
                 <h1>hh<span className="text-[green]">.</span></h1>
             </a>
-            <HamburgerMenu />
 
-            <nav className="hidden sm:block sm:flex sm:justify-between sm:gap-[8px]">
+            <div className="hidden sm:block sm:justify-between sm:gap-[8px]">
                 <a href="/login" className="border-[1px] border-[#75716B] px-[40px] py-[12px] rounded-[999px] gap-[6px]">
                     Log in
                 </a>
                 <a href="/signup" className="bg-[#26231E] text-[#ffffff] border-[1px] border-[#75716B] px-[40px] py-[12px] rounded-[999px] gap-[6px]">
                     Sign up
                 </a>
-            </nav>
+            </div>
+            <DropdownMenu>
+                <DropdownMenuTrigger className="sm:hidden focus:outline-none">
+                    <Menu />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="sm:hidden w-screen rounded-none mt-4 flex flex-col gap-6 py-10 px-6">
+                    <a
+                        href="/login"
+                        className="px-8 py-4 rounded-full text-center text-foreground border border-foreground hover:border-muted-foreground hover:text-muted-foreground transition-colors"
+                    >
+                        Log in
+                    </a>
+                    <a
+                        href="/signup"
+                        className="px-8 py-4 bg-foreground text-center text-white rounded-full hover:bg-muted-foreground transition-colors"
+                    >
+                        Sign up
+                    </a>
+                </DropdownMenuContent>
+            </DropdownMenu>
         </nav>
 
     )
